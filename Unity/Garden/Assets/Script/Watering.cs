64aff3db7fe1909b83bf9e2c313039cd68d5dd79
0mNom <62939121+0mNom@users.noreply.github.com>
2021-01-21 20:26:35 +0100

    watering stuffs

---
+++ Unity/Garden/Assets/Script/Watering.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watering : MonoBehaviour
+{
+    public GameObject particle;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void water()
+    {
+        Instantiate(particle);
+    }
+}
