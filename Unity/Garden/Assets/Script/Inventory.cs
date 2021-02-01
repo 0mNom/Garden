283195f51c8738d4b5829ecf2529dbfc83670a92
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-01 22:33:33 +0100

    Revert "i mean it works but why"
    This reverts commit f0404e6d533f060c938132bc37d60a3a2761ca2f.

--- Unity/Garden/Assets/Script/Inventory.cs
+++ Unity/Garden/Assets/Script/Inventory.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Inventory : MonoBehaviour
 {
     public int pot1, pot2, pot3, pot4, pot5, pot6, pot7; //number of each kind of pot
-    public TMP_Text t_pot1, t_pot2, t_pot3, t_pot4, t_pot5, t_pot6, t_pot7;
     public BagScript bag;
     public GameObject error;
 
@@ -18,13 +16,7 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t_pot1.text = pot1.ToString();
-        t_pot2.text = pot2.ToString();
-        t_pot3.text = pot3.ToString();
-        t_pot4.text = pot4.ToString();
-        t_pot5.text = pot5.ToString();
-        t_pot6.text = pot6.ToString();
-        t_pot7.text = pot7.ToString();
+        
     }
     public void buyP1()
     {
@@ -52,13 +44,13 @@ public class Inventory : MonoBehaviour
     }
     public void buyP3()
     {
-        if (bag.Gems < 1)
+        if (bag.Gems < 12)
         {
             StartCoroutine("errr");
         }
         else
         {
-            bag.Gems -= 1;
+            bag.Gems -= 12;
             pot3++;
         }
     }
--- Unity/Garden/Assets/Script/PotSkins.cs
+++ Unity/Garden/Assets/Script/PotSkins.cs
@@ -23,89 +23,49 @@ public class PotSkins : MonoBehaviour
 
     public void skin1()
     {
-        if (inv.pot1 < 1)
-        {
-            inv.StartCoroutine("errr");
+        //if (inv.pot1)
 
-        }
-        else
-        {
-            inv.pot1--;
-
-
-            if (popUp.Tag == "1") places.sp1 = 1;
-            if (popUp.Tag == "2") places.sp2 = 1;
-            if (popUp.Tag == "3") places.sp3 = 1;
-            if (popUp.Tag == "4") places.sp4 = 1;
-            if (popUp.Tag == "5") places.sp5 = 1;
-            if (popUp.Tag == "6") places.sp6 = 1;
-            if (popUp.Tag == "7") places.sp7 = 1;
-            if (popUp.Tag == "8") places.sp8 = 1;
-            if (popUp.Tag == "9") places.sp9 = 1;
-            if (popUp.Tag == "10") places.sp10 = 1;
-            sendinfo();
-        }
-        
-
-
-
+        if (popUp.Tag == "1") places.sp1 = 1;
+        if (popUp.Tag == "2") places.sp2 = 1;
+        if (popUp.Tag == "3") places.sp3 = 1;
+        if (popUp.Tag == "4") places.sp4 = 1;
+        if (popUp.Tag == "5") places.sp5 = 1;
+        if (popUp.Tag == "6") places.sp6 = 1;
+        if (popUp.Tag == "7") places.sp7 = 1;
+        if (popUp.Tag == "8") places.sp8 = 1;
+        if (popUp.Tag == "9") places.sp9 = 1;
+        if (popUp.Tag == "10") places.sp10 = 1;
+        sendinfo();
 
     }
     public void skin2()
     {
-
-        if (inv.pot2 < 1)
-        {
-            inv.StartCoroutine("errr");
-
-        }
-        else
-        {
-            inv.pot2--;
-
-            if (popUp.Tag == "1") places.sp1 = 2;
-            if (popUp.Tag == "2") places.sp2 = 2;
-            if (popUp.Tag == "3") places.sp3 = 2;
-            if (popUp.Tag == "4") places.sp4 = 2;
-            if (popUp.Tag == "5") places.sp5 = 2;
-            if (popUp.Tag == "6") places.sp6 = 2;
-            if (popUp.Tag == "7") places.sp7 = 2;
-            if (popUp.Tag == "8") places.sp8 = 2;
-            if (popUp.Tag == "9") places.sp9 = 2;
-            if (popUp.Tag == "10") places.sp10 = 2;
-            sendinfo();
-        }
-       
-
-        
+        if (popUp.Tag == "1") places.sp1 = 2;
+        if (popUp.Tag == "2") places.sp2 = 2;
+        if (popUp.Tag == "3") places.sp3 = 2;
+        if (popUp.Tag == "4") places.sp4 = 2;
+        if (popUp.Tag == "5") places.sp5 = 2;
+        if (popUp.Tag == "6") places.sp6 = 2;
+        if (popUp.Tag == "7") places.sp7 = 2;
+        if (popUp.Tag == "8") places.sp8 = 2;
+        if (popUp.Tag == "9") places.sp9 = 2;
+        if (popUp.Tag == "10") places.sp10 = 2;
+        sendinfo();
 
     }
     public void skin3()
     {
-        if (inv.pot3 < 1)
-        {
-
-            inv.StartCoroutine("errr");
-
-
-        }
-        else
-        {
-            inv.pot3--;
-
-            if (popUp.Tag == "1") places.sp1 = 3;
-            if (popUp.Tag == "2") places.sp2 = 3;
-            if (popUp.Tag == "3") places.sp3 = 3;
-            if (popUp.Tag == "4") places.sp4 = 3;
-            if (popUp.Tag == "5") places.sp5 = 3;
-            if (popUp.Tag == "6") places.sp6 = 3;
-            if (popUp.Tag == "7") places.sp7 = 3;
-            if (popUp.Tag == "8") places.sp8 = 3;
-            if (popUp.Tag == "9") places.sp9 = 3;
-            if (popUp.Tag == "10") places.sp10 = 3;
-            sendinfo();
-        }
-
+        if (popUp.Tag == "1") places.sp1 = 3;
+        if (popUp.Tag == "2") places.sp2 = 3;
+        if (popUp.Tag == "3") places.sp3 = 3;
+        if (popUp.Tag == "4") places.sp4 = 3;
+        if (popUp.Tag == "5") places.sp5 = 3;
+        if (popUp.Tag == "6") places.sp6 = 3;
+        if (popUp.Tag == "7") places.sp7 = 3;
+        if (popUp.Tag == "8") places.sp8 = 3;
+        if (popUp.Tag == "9") places.sp9 = 3;
+        if (popUp.Tag == "10") places.sp10 = 3;
+        sendinfo();
 
     }
      public void skin4()
