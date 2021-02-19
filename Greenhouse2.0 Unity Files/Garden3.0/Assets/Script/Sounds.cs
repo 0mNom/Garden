ad6c83b57cede4df7487583b4c6c5467db235eaf
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-19 14:51:38 +0100

    Update Sounds.cs

--- Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/Sounds.cs
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/Sounds.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Sounds : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip s_PotPlaced, s_buy, s_buyMoney, s_buySeeds, s_ScreenSwosh, BG_music, s_pop, s_openBag, s_water;
+
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+
     void Start()
     {
-        
+        gameObject.AddComponent<AudioSource>();
+        source.clip = BG_music;
+        source.playOnAwake = false;
+
+        source.volume = 0.5f;
+
+        source.clip = BG_music;
+        source.PlayOneShot(BG_music);
     }
 
     // Update is called once per frame
@@ -15,4 +26,51 @@ public class Sounds : MonoBehaviour
     {
         
     }
+
+
+    public void potPlaced()
+    {
+        source.clip = s_PotPlaced;
+        source.PlayOneShot(s_PotPlaced);
+    }
+
+    public void buy()
+    {
+        source.clip = s_buy;
+        source.PlayOneShot(s_buy);
+    }
+
+    public void BuyMoney()
+    {
+        source.clip = s_buyMoney;
+        source.PlayOneShot(s_buyMoney);
+    }
+
+    public void BuySeeds()
+    {
+        source.clip = s_buySeeds;
+        source.PlayOneShot(s_buySeeds);
+    }
+
+    public void Swoosh()
+    {
+        source.clip = s_ScreenSwosh;
+        source.PlayOneShot(s_ScreenSwosh);
+    }
+
+     public void pop()
+    {
+        source.clip = s_pop;
+        source.PlayOneShot(s_pop);
+    }
+
+
+     public void pop()
+    {
+        source.clip = s_pop;
+        source.PlayOneShot(s_pop);
+    }
+
+
+
 }
