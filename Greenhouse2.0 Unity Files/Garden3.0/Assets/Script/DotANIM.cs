e6ab7d57255ec15ddfa8ddf2491c4c00a25184ae
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-21 18:54:25 +0100

    menu anim pt 1

--- Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/DotANIM.cs
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/DotANIM.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 
 
 public class DotANIM : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RectTransform menu, gamePannel, menuCanvus;
     void Start()
     {
-        DOTween.Init();
+        menu.DOAnchorPos(Vector2.zero,0.25f);
     }
 
     // Update is called once per frame
@@ -22,5 +24,16 @@ public class DotANIM : MonoBehaviour
     public void punch()
     {
         this.transform.DOPunchScale(new Vector3 ( 1,1,1), 1f,2,1);
+    }
+
+    public void buttonshake()
+    {
+       // this.DOShakeAnchorPos(1f, 1f, 2, 0.8f, false, false);
+    }
+
+    public void offmenu()
+    {
+        menuCanvus.DOAnchorPos(new Vector2(-1200, 0), 0.25f);
+        gamePannel.DOAnchorPos(new Vector2(0, 0), 0.25f);
     }
 }
