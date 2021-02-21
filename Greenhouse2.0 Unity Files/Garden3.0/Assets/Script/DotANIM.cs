38ef8a5ad0f6bf3ca0dc23bb26e6eb1a02d8f914
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-21 18:28:44 +0100

    i love the BG

---
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/DotANIM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+
+
+public class DotANIM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void punch()
+    {
+        this.transform.DOPunchScale(new Vector3 ( 1,1,1), 1f,2,1);
+    }
+}
