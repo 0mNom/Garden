afc0e9f8ed7a8b530676dff3b81f2f28f76798fb
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-21 21:23:04 +0100

    bedtime

--- Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/Loader.cs
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/Loader.cs
@@ -122,6 +122,7 @@ public class Loader : MonoBehaviour
     {
         auto = true;
         SaveGame();
+        Debug.Log("Saved");
         yield return new WaitForSeconds(30f);
         auto = false;
     }
