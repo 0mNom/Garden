fab99e0705c92c9985258ce8168c8b65052e280f
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-22 17:26:01 +0100

    changes

---
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/PopUpAds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpAds : MonoBehaviour
+{
+    public GameObject ad1;
+    public bool adOn = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!adOn) StartCoroutine("PopUp");
+
+
+    }
+
+    IEnumerator PopUp()
+    {
+        adOn = true;
+        yield return new WaitForSeconds(20);
+        ad1.SetActive(true);
+    }
+
+
+
+}
