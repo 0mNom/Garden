8bbd4a1f0fbe586e6f0bfe842e41ba58ee4b8199
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-23 15:48:54 +0100

    tiens

--- Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/DotANIM.cs
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/DotANIM.cs
@@ -12,6 +12,9 @@ public class DotANIM : MonoBehaviour
     public RectTransform menu, gamePannel, menuCanvus, logo, blackpannel, seedBag, seeds,marketButton, market, Pmarket, PmarketButton, nowP, error;
     public GameObject conti,popUp ;
 
+    public RectTransform WheatScreen, AppleScreen, TomatoScreen, EggplantScreen, PearScreen, SunflowerScreen, CherryScreen, AvoScreen, KiwiScreen;
+    public RectTransform p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16;
+
     void Start()
     {
         // menu.DOAnchorPos(Vector2.zero,0.25f);
@@ -34,6 +37,255 @@ public class DotANIM : MonoBehaviour
             }
         }*/
     }
+
+
+    /// <summary>
+    /// start of pot market pannel pop up anims
+    /// </summary>
+    public void p1in()
+    {
+        p1.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p1out()
+    {
+        p1.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p2in()
+    {
+        p2.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p2out()
+    {
+        p2.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+     public void p3in()
+    {
+        p3.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p3out()
+    {
+        p3.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p4in()
+    {
+        p4.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p4out()
+    {
+        p4.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+
+    public void p5in()
+    {
+        p5.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p5out()
+    {
+        p5.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p6in()
+    {
+        p6.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p6out()
+    {
+        p6.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+     public void p7in()
+    {
+        p7.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p7out()
+    {
+        p7.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p8in()
+    {
+        p8.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p8out()
+    {
+        p8.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+
+    public void p9in()
+    {
+        p9.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p9out()
+    {
+        p9.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p10in()
+    {
+        p10.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p10out()
+    {
+        p10.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+     public void p11in()
+    {
+        p11.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p11out()
+    {
+        p11.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p12in()
+    {
+        p12.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p12out()
+    {
+        p12.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+
+    public void p13in()
+    {
+        p13.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p13out()
+    {
+        p13.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p14in()
+    {
+        p14.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p14out()
+    {
+        p14.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+
+    public void p15in()
+    {
+        p15.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p15out()
+    {
+        p15.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+    public void p16in()
+    {
+        p16.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void p16out()
+    {
+        p16.DOAnchorPos(new Vector2(0, -2000), 0.4f);
+    }
+
+    /// <summary>
+    /// start of plant pannel pop up anims
+    /// </summary>
+    public void wheatin()
+    {
+        WheatScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void wheatout()
+    {
+        WheatScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+    public void applein()
+    {
+        AppleScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void appleout()
+    {
+        AppleScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+    
+    public void tomin()
+    {
+        TomatoScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void tomout()
+    {
+        TomatoScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+    public void eggin()
+    {
+        EggplantScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void eggout()
+    {
+        EggplantScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+
+    
+    public void pearin()
+    {
+        PearScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void pearout()
+    {
+        PearScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+    public void sunin()
+    {
+        SunflowerScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void sunout()
+    {
+        SunflowerScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+    
+    public void cherrtin()
+    {
+        CherryScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void cherryout()
+    {
+        CherryScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+    public void avoin()
+    {
+        AvoScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void avoout()
+    {
+        AvoScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+    public void kiwiin()
+    {
+        KiwiScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void kiwiout()
+    {
+       KiwiScreen.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+    }
+
+
+
+    /// <summary>
+    /// start of other anims
+    /// </summary>
+
+
+
 
     public void errorOn()
     {
