3344beda2aaf95150fe4a05281e53ba42ccc8ccc
0mNom <62939121+0mNom@users.noreply.github.com>
2021-02-26 21:25:53 +0100

    eyo

--- Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/PotPlaces.cs
+++ Greenhouse2.0 Unity Files/Garden3.0/Assets/Script/PotPlaces.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PotPlaces : MonoBehaviour
 {
     // Start is called before the first frame update
-    public bool p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 = true ;  //POSSIBLE POT PLACES AND IF THEY HAVE BEEN ACTIVED:BOUGHT
+    public bool p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 = true;  //POSSIBLE POT PLACES AND IF THEY HAVE BEEN ACTIVED:BOUGHT
     public int sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10; //skins on the active pots 
     public float pp1, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10; //pot planted in x pot
     public Sprite pousseBle, evolutionBle1, evolutionBle2, evolutionBle3;
@@ -19,7 +19,7 @@ public class PotPlaces : MonoBehaviour
     public Sprite pousseKiwi, evolutionKiwi1, evolutionKiwi2, evolutionKiwi3;
     public Sprite Skin1, Skin2, Skin3, Skin4, Skin5, Skin6, Skin7, Skin8, Skin9, Skin10, Skin11, Skin12, Skin13, Skin14, Skin15, Skin16;
     public Sprite Skin1Shine, Skin2Shine, Skin3Shine, Skin4Shine, Skin5Shine, Skin6Shine, Skin7Shine, Skin8Shine, Skin9Shine, Skin10Shine, Skin11Shine, Skin12Shine, Skin13Shine, Skin14Shine, Skin15Shine, Skin16Shine;
-    public SpriteRenderer spriteRendererP1,spriteRendererP2, spriteRendererP3;
+    public SpriteRenderer spriteRendererP1, spriteRendererP2, spriteRendererP3;
     public SpriteRenderer spriteRendererPot1, spriteRendererPot2, spriteRendererPot3;
     public SpriteRenderer spriteRendererPot1Shine, spriteRendererPot2Shine, spriteRendererPot3Shine;
     public Animator potAnim;
@@ -28,14 +28,15 @@ public class PotPlaces : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    
 
     public void loadpot(string tag)
     {
